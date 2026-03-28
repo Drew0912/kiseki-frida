8d109642f63d8f6a2ea55e1dfa1404140661c333
Andrew Lee <leea0912@gmail.com>
2026-03-28 21:49:57 +0000

    Neatened logger.

--- src/ed8/ed85/mods/logger.ts
+++ src/ed8/ed85/mods/logger.ts
@@ -2,19 +2,19 @@ import { Addrs } from "../addrs";
 import { Interceptor2 } from "../../../utils";
 import * as utils from "../../../utils";
 
-enum LoggerLevel {
+export enum LoggerLevel {
     NoInfo = 0,
-    Printf = 1,
-    Logger2 = 2, // less info, more accurate
+    OutputPrintf = 1, // Recommended.
+    OutputDebugString = 2, // Less info, more accurate.
 }
 
-let loggerLevel = LoggerLevel.Printf;
-export function setLoggerLevel(value: number) {
+let loggerLevel = LoggerLevel.OutputPrintf;
+export function setLoggerLevel(value: LoggerLevel) {
     loggerLevel = value;
 }
 
 // More accurate logger but gives less information.
-function Logger2() {
+function hookOutputDebugString() {
     // Not needed?
     // Interceptor2.jmp(
     //     Addrs.Logger.Ansi2UTF8,
@@ -31,7 +31,7 @@ function Logger2() {
     const Logger_Output = Interceptor2.jmp(
         Addrs.Logger.OutputDebugStringW,
         function(self: NativePointer, level: NativePointer, buffer: NativePointer) {
-            if (loggerLevel == LoggerLevel.Logger2) {
+            if (loggerLevel == LoggerLevel.OutputDebugString) {
                 const buf = buffer.readUtf8String()!;
                 if (buf.substring(buf.length - 1) == '\n') {
                     utils.log(buf.substring(0, buf.length - 1));
@@ -45,48 +45,52 @@ function Logger2() {
     );
 }
 
-function hookLoggerPrintf() {
+function hookOutputPrintf() {
     const regex = /%([-+ #0]*)(\d+)?(\.(\d+))?([hlL]?)([diuoxXfFeEgGaAcspn%])/g;
     const basicReg = /%([dfsxXp%])/g;
     // Param1 = level, Param2 = format, Param3... = ...args.
     Interceptor.attach(Addrs.Logger.Output_Printf, {
         onEnter(args) {
-            const format = args[1].readUtf8String()!.trim();
+            if (loggerLevel == LoggerLevel.OutputPrintf){
+                const format = args[1].readUtf8String()!.trim();
 
-            // const matches = format.match(regex) || []; // Simple regex for types
-            // matches.forEach((specifier, index) => {
-            //     const val = args[index + 2]; // Offset by 2 because 0=dest, 1=format
-            //     utils.log(`Arg ${index}: ${specifier} -> ${val}, format = ${format}`);
-            // });
+                // const matches = format.match(regex) || []; // Simple regex for types
+                // matches.forEach((specifier, index) => {
+                //     const val = args[index + 2]; // Offset by 2 because 0=dest, 1=format
+                //     utils.log(`Arg ${index}: ${specifier} -> ${val}, format = ${format}`);
+                // });
 
-            let i = 2;
-            let formatted = format.replace(regex, (match, flags, width, precisionFull, precision, length, specifier) => {
-                if (specifier === '%') return '%';
-                const arg = args[i++];
+                let i = 2;
+                let formatted = format.replace(regex, (match, flags, width, precisionFull, precision, length, specifier) => {
+                    if (specifier === '%') return '%';
+                    const arg = args[i++];
 
-                switch (specifier) {
-                    case 'd': return arg.toInt32()!.toString();
-                    case 's': return arg.readUtf8String()!;
-                    case 'x': return arg.toString()!.slice(2, 10);
-                    case 'f':
-                        let value = utils.hexDoubletoDouble(arg.toString()!);
-                        if (precision !== undefined) {
-                            return value.toFixed(parseInt(precision))
+                    switch (specifier) {
+                        case 'd': return arg.toInt32()!.toString();
+                        case 's': return arg.readUtf8String()!;
+                        case 'x': return arg.toString()!.slice(2, 10);
+                        case 'f':
+                            let value = utils.hexDoubletoDouble(arg.toString()!);
+                            if (precision !== undefined) {
+                                return value.toFixed(parseInt(precision))
+                            }
+                            return value.toString();
+                        default:
+                            utils.log(`Unknown format: ${match}`);
+                            return match;
                         }
-                        return value.toString();
-                    default:
-                        utils.log(`Unknown format: ${match}`);
-                        return match;
-                }
-            });
-            utils.log(`${formatted}`);
-        },
+                });
+                
+                utils.log(`${formatted}`);
+            }
+        }
     });
 }
 
-export function outputDebugInfo(level: number) {
+
+export function setupOutputDebugInfo(level: number) {
     loggerLevel = level;
     // Do hooks
-    hookLoggerPrintf()
-    Logger2()
+    hookOutputPrintf()
+    hookOutputDebugString()
 }
